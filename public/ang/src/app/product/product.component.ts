d7d533ec6e1c5353b3b0ff050b65ceb18ce7a4c3
Sergei Nikonov <sergei.nikonov@yahoo.com>
2017-12-23 17:10:20 +0200

    product => data is being loaded from the db request

--- public/ang/src/app/product/product.component.ts
+++ public/ang/src/app/product/product.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+import * as rootReducer from '../redux/reducers/app.reducer';
+import * as loaderActions from '../redux/actions/loader.actions';
+
+import { AppService } from '../core/app.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  product: any = null;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private appService: AppService, private store: Store<rootReducer.State>, private activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe((params: Params) => this.loadData(params['productId']));
   }
 
+  ngOnInit() {
+    this.store.dispatch(new loaderActions.LoaderSetStateAction(true));
+  }
+
+  loadData(productId) {
+    this.appService.getProductData(productId).subscribe(
+      (resp) => {
+        this.product = resp;
+        this.store.dispatch(new loaderActions.LoaderSetStateAction(false));
+      },
+      (err) => {
+        this.store.dispatch(new loaderActions.LoaderSetStateAction(false));
+      }
+    );
+  }
 }
